6429c3ac9cd092b4cf34d0782e3d60eda7097769
dmitry-sol <dmitry-sol@mail.ru>
2024-07-17 13:03:14 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,9 @@
+# Homework 2
+# https://github.com/dmitry-sol/homework2
+
+home_work_number = 12
+hrs_spent = 1.5
+course_name = 'Python'
+time_for_one_work = hrs_spent / home_work_number
+print('Курс: ' + str(course_name) + ', всего задач:' + str(home_work_number) + ', затрачено часов:'
+      + str(hrs_spent) + ', среднее время выполнения', time_for_one_work, 'часа.')
---
+++ Homework3.py
@@ -0,0 +1,10 @@
+#Homework3
+
+name = 'Dmitry'
+print('Name:', name)
+age = 55
+print('Age:', age)
+age = age + 30
+print('New Age:', age)
+is_student = 'True'
+print('Is Student:', is_student)
