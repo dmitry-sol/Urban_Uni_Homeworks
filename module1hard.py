bd57985fb5a5f8f1f23de9cc6803b60e8fc3ebf7
dmitry-sol <dmitry-sol@mail.ru>
2024-07-21 14:03:35 +0300

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,18 @@
+# module1hard
+
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+
+count = len(grades)
+average_grade = []
+i = 0
+
+for i in range(count):
+    average_grade.append(round(sum(grades[i]) / len(grades[i]),2))
+    i += 1
+
+students = list(students)
+students.sort()
+
+average_grade_dict = dict(zip(students, average_grade))
+print('Average grade of students:', average_grade_dict)
