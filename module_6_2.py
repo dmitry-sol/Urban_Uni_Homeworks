5da1fa0cf3c1ea024dcdedc8edfcc1d9b5f86a65
dmitry-sol <dmitry-sol@mail.ru>
2024-08-29 08:12:36 +1100

    Add files via upload

---
+++ module_6_2.py
@@ -0,0 +1,53 @@
+# module_6_2
+# Доступ к свойствам родителя. Переопределение свойств
+
+class Vehicle:
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+
+    def __init__(self, owner, __model, __engine_power, __color):
+        self.owner = owner
+        self.__model = __model
+        self.__engine_power = __engine_power
+        self.__color = __color
+        # self.new_color = ''
+
+    def get_model(self):
+        return print(f'Модель: {self.__model}')
+
+    def get_horsepower(self):
+        return print(f'Мощность: {self.__engine_power}')
+
+    def get_color(self):
+        return print(f'Цвет: {self.__color}')
+
+    def print_info(self):
+        self.get_model()
+        self.get_horsepower()
+        self.get_color()
+        return print(f'Владелец: {self.owner}')
+
+    def set_color(self, new_color):
+        self.new_color = new_color
+        if new_color.lower() in self.__COLOR_VARIANTS:
+            self.__color = new_color
+        else:
+            print(f'Нельзя сменить цвет на {new_color}')
+
+
+class Sedan(Vehicle):
+    __PASSENGERS_LIMIT = 5
+
+
+# Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 500, 'blue')
+
+# Изначальные свойства
+vehicle1.print_info()
+
+# Меняем свойства (в т.ч. вызывая методы)
+vehicle1.set_color('Pink')
+vehicle1.set_color('BLACK')
+vehicle1.owner = 'Vasyok'
+
+# Проверяем что поменялось
+vehicle1.print_info()
