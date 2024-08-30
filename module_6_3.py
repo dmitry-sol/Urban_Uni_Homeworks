09cf47025d0336659e871b76f4e797d8716f0e2b
dmitry-sol <dmitry-sol@mail.ru>
2024-08-31 06:38:52 +1100

    Update module_6_3.py

--- module_6_3.py
+++ module_6_3.py
@@ -2,39 +2,40 @@
 # "Множественное наследование"
 
 class Horse:
-    def __init__(self, x_distance=0, sound='Frrr', y_distance=0):
-        super().__init__(y_distance)
-        self.x_distance = x_distance
+    def __init__(self):
+        self.x_distance = 0
+        self.sound = 'Frrrr'
 
     def run(self, dx):
-        self.x_distance = self.x_distance + dx
-        return self.x_distance
+        self.x_distance += dx
 
 
 class Eagle:
-    def __init__(self, y_distance=0, sound='I train, eat, sleep, and repeat'):
-        self.y_distance = y_distance
-        self.sound = sound
+    def __init__(self):
+        self.y_distance = 0
+        self.sound = 'I train, eat, sleep, and repeat'
 
     def fly(self, dy):
-        self.y_distance = self.y_distance + dy
-        return self.y_distance
+        self.y_distance += dy
 
 
 class Pegasus(Horse, Eagle):
+    def __init__(self):
+        Horse.__init__(self)
+        Eagle.__init__(self)
 
     def move(self, dx, dy):
-        return (self.run(dx), self.fly(dy))
+        super().run(dx)
+        super().fly(dy)
 
     def get_pos(self):
-        return (self.x_distance, self.y_distance)
+        return self.x_distance, self.y_distance
 
     def voice(self):
-        return print(self.sound)
+        print(self.sound)
 
 
 p1 = Pegasus()
-# print(Pegasus.mro())
 
 print(p1.get_pos())
 p1.move(10, 15)
