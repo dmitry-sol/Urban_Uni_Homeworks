7d51d73b9ccd032c8401c87b0910268476519a4a
dmitry-sol <dmitry-sol@mail.ru>
2024-09-06 08:33:16 +1100

    Update module6hard.py

--- module6hard.py
+++ module6hard.py
@@ -15,7 +15,7 @@ class Figure:
         return self.__color
 
     def __is_valid_sides(self, sides):
-        if type(sum(sides)) is int and min(sides) >=0 and len(list(sides)) == self.sides_count:
+        if type(sum(sides)) is int and min(sides) >= 0 and len(list(sides)) == self.sides_count:
             return True
         else:
             return False
@@ -50,6 +50,7 @@ class Figure:
     def get_sides(self):
         return self.__sides
 
+
 class Circle(Figure):
     sides_count = 1
 
@@ -59,10 +60,10 @@ class Circle(Figure):
             self.sides = sides
         else:
             self.sides = [1]
-        self.radius = self.sides[0] / (2 * pi)
 
     def get_square(self):
-        square = pi * self.radius ** 2
+        radius = self.sides[0] / (2 * pi)
+        square = pi * radius ** 2
         return square
 
     def __len__(self):
@@ -97,6 +98,7 @@ class Triangle(Figure):
         self.sides = super().get_sides()
         return list(self.sides)
 
+
 class Cube(Figure):
     sides_count = 1
 
@@ -113,14 +115,16 @@ class Cube(Figure):
 
     def get_sides(self):
         self.sides = super().get_sides()
-        return list(self.sides *12)
+        return list(self.sides * 12)
 
 
 circle1 = Circle((200, 150, 100), 10)  # (Цвет, стороны)
 circle2 = Circle((120, 200, 250), 10, 20)  # (Цвет, стороны)
-cube1 = Cube((222, 35, 130), 6) # (Цвет, стороны)
-triangle1 = Triangle((50, 100, 150), 5, 5, 7) # (Цвет, стороны)
+cube1 = Cube((222, 35, 130), 6)
+triangle1 = Triangle((50, 100, 150), 5, 5, 7)
 
+print()
+print(' = ПРОВЕРКА В РАМКАХ ЗАДАЧИ =')
 # Проверка на изменение цветов:
 circle1.set_color(55, 66, 77)  # Изменится
 print(circle1.get_color())
@@ -139,32 +143,83 @@ print(len(circle1))
 # Проверка объёма (куба):
 print(cube1.get_volume())
 print()
-
-
-# Площадь круга
-print(circle1.get_square())
 print()
 
-# Проверка треугольника
-triangle1.set_color(77, 77, 77)  # Изменится
-print(triangle1.get_color())
-triangle1.set_sides(5, 3, 12, 4, 5)  # Не изменится
-print(triangle1.get_sides())
-triangle1.set_sides(50, 50, 70)  # Изменится
-print(triangle1.get_sides())
-# Периметр треугольника
-print(len(triangle1))
-# Площадь треугольника
-print(triangle1.get_square())
-
-# Проверка куба
-cube1.set_color(88, 88, 88)  # Изменится
-print(cube1.get_color())
-cube1.set_sides(100, 3, 12, 4, 5)  # Не изменится
-cube1.set_sides(50)  # Изменится
-print(cube1.get_sides())
+print(' = РАСШИРЕННАЯ ПРОВЕРКА =')
+print()
+print(' = ПРОВЕРКА КРУГА =')
+print('Цвета круга:', circle1.get_color())
+print('Стороны круга:', circle1.get_sides())
+print('Площадь круга:', circle1.get_square())
+print('Периметр круга:', len(circle1))
 print()
 
-# print(circle1.__dict__)
-# print(triangle1.__dict__)
-# print(cube1.__dict__)
+circle1.set_color(300, 100, 100)  # Изменится
+print('Меняем цвет круга неправильно и проверяем, изменится ли:', circle1.get_color())
+circle1.set_color(100, 100, 101)  # Изменится
+print('Меняем цвет круга правильно и проверяем, изменится ли:', circle1.get_color())
+print()
+
+print('Меняем стороны круга неправильно и проверяем изменится ли:', circle1.set_sides(12, 45, 32))
+print('Стороны круга:', circle1.get_sides())
+print('Периметр круга:', len(circle1))
+print('Площадь круга:', circle1.get_square())
+print()
+
+print('Меняем стороны круга правильно и проверяем изменится ли:', circle1.set_sides(33))
+print('Стороны круга:', circle1.get_sides())
+print('Периметр круга:', len(circle1))
+print('Площадь круга:', circle1.get_square())
+print()
+
+print(' = ПРОВЕРКА ТРЕУГОЛЬНИКА =')
+print('Цвета треугольника:', triangle1.get_color())
+print('Стороны треугольника:', triangle1.get_sides())
+print('Периметр треугольника:', len(triangle1))
+print('Площадь треугольника:', triangle1.get_square())
+print()
+
+triangle1.set_color(100, 300, 100)  # Изменится
+print('Меняем цвет круга неправильно и проверяем, изменится ли:', triangle1.get_color())
+triangle1.set_color(100, 100, 102)  # Изменится
+print('Меняем цвет круга правильно и проверяем, изменится ли:', triangle1.get_color())
+print()
+
+print('Меняем стороны треугольника неправильно и проверяем изменится ли:', triangle1.set_sides(12, 45, 32, 55))
+print('Стороны круга:', triangle1.get_sides())
+print('Площадь круга:', triangle1.get_square())
+print('Периметр круга:', len(triangle1))
+print()
+
+print('Меняем стороны треугольника правильно и проверяем изменится ли:', triangle1.set_sides(50, 50, 70))
+print('Стороны круга:', triangle1.get_sides())
+print('Площадь круга:', triangle1.get_square())
+print('Периметр круга:', len(triangle1))
+print()
+
+print(' = ПРОВЕРКА КУБА =')
+print('Цвета куба:', cube1.get_color())
+print('Стороны куба:', cube1.get_sides())
+print('Объем куба:', cube1.get_volume())
+print()
+
+cube1.set_color(100, 100, 300)  # Изменится
+print('Меняем цвет куба неправильно и проверяем, изменится ли:', cube1.get_color())
+cube1.set_color(100, 100, 103)  # Изменится
+print('Меняем цвет куба правильно и проверяем, изменится ли:', cube1.get_color())
+print()
+
+print('Меняем стороны куба неправильно и проверяем изменится ли:', cube1.set_sides(50, 50))
+print('Стороны куба:', cube1.get_sides())
+print('Объем куба:', cube1.get_volume())
+print()
+
+print('Меняем стороны куба правильно и проверяем изменится ли:', cube1.set_sides(100))
+print('Стороны куба:', cube1.get_sides())
+print('Объем куба:', cube1.get_volume())
+print()
+print()
+
+print(circle1.__dict__)
+print(triangle1.__dict__)
+print(cube1.__dict__)
