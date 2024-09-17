ef8171a1f348496b0897c3bc14447d9dd194a24c
dmitry-sol <dmitry-sol@mail.ru>
2024-09-17 13:13:52 +1100

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,13 @@
+# module_8_1
+# Try и Except
+
+def add_everything_up(a, b):
+    try:
+        return round(a + b, 3)
+    except TypeError:
+        return str(a) + str(b)
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
