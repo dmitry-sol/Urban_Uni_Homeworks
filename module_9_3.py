3223f1f8b01799d49e3c67669e7c868505ae3f9d
dmitry-sol <dmitry-sol@mail.ru>
2024-09-19 10:38:46 +1100

    Update module_9_3.py

--- module_9_3.py
+++ module_9_3.py
@@ -6,7 +6,7 @@ second = ['Строка', 'Урбан', 'Компьютер']
 
 first_result = (len(x) - len(y) for x, y in zip(first, second) if len(x) != len(y))
 second_result = (len(first[i]) == len(second[i]) for i in range(min(len(first), len(second))))
-second_result_2_way = map(lambda x,y: len(x) == len(y), first, second)
+second_result_2_way = map(lambda x, y: len(x) == len(y), first, second)
 
 print(list(first_result))
 print(list(second_result))
