4f921f6952eeca3ac660a28aa02c6e31faa01151
dmitry-sol <dmitry-sol@mail.ru>
2024-09-18 20:41:34 +1100

    Update module_9_2.py

--- module_9_2.py
+++ module_9_2.py
@@ -4,9 +4,9 @@
 first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
 second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
 
-first_result = [len(x) for x in first_strings if len(x)>=5]
-second_result = [(x, y) for x in first_strings for y in second_strings if len(x)==len(y)]
-third_result = {word: len(word) for word in first_strings + second_strings if not len(word)%2}
+first_result = [len(x) for x in first_strings if len(x) >= 5]
+second_result = [(x, y) for x in first_strings for y in second_strings if len(x) == len(y)]
+third_result = {word: len(word) for word in first_strings + second_strings if not len(word) % 2}
 
 print(first_result)
 print(second_result)
