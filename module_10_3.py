bc27346d221ee7331adc345228f73b03051c88d5
dmitry-sol <dmitry-sol@mail.ru>
2024-10-06 01:41:18 +1100

    Update module_10_3.py

--- module_10_3.py
+++ module_10_3.py
@@ -13,42 +13,39 @@ class Bank:
         self.sleep_duration = sleep_duration
         self.balance = balance
         self.lock = lock
-        self.lock_counter_1 = 0
-        self.lock_counter_2 = 0
+        self.lock_counter = 0
+        self.end_of_thread_1 = False
 
     def deposit(self):
         for transaction in range(self.transactions):
             any_transaction = random.randint(50, 500)
             self.balance += any_transaction
-            print(f'{transaction + 1}. Пополнение: {any_transaction}. Баланс: {self.balance}')
+            print(f'{transaction + 1}+. Пополнение: {any_transaction}. Баланс: {self.balance}')
+            if transaction == 99:
+                self.end_of_thread_1 = True
             if self.balance >= 500:
                 if self.lock.locked():
                     self.lock.release()
-                    self.lock_counter_1 += 1
-                    print(f' - Замок открылся {self.lock_counter_1}-й раз')
+                    print(f'     = Замок открылся {self.lock_counter}-й раз')
             else:
                 continue
             sleep(self.sleep_duration)
 
     def take(self):
-
         for transaction in range(self.transactions):
-            try:
-                any_transaction = random.randint(50, 500)
-                print(f' - Запрос на {any_transaction}')
-                if any_transaction <= self.balance:
-                    self.balance -= any_transaction
-                    print(f'{transaction + 1}. Снятие: {any_transaction}. Баланс: {self.balance}.')
+            any_transaction = random.randint(50, 500)
+            print(f'     = Запрос на {any_transaction}')
+            if any_transaction <= self.balance:
+                self.balance -= any_transaction
+                print(f'{transaction + 1}-. Снятие: {any_transaction}. Баланс: {self.balance}.')
+            else:
+                print(f'{transaction + 1}-. Запрос отклонён, недостаточно средств')
+                if self.end_of_thread_1:
+                    continue
                 else:
-                    print(f'{transaction + 1}. Запрос отклонён, недостаточно средств')
                     self.lock.acquire()
-                    self.lock_counter_2 += 1
-                    print(f' - Замок закрылся {self.lock_counter_2}-й раз')
-            finally:
-                if self.lock.locked():
-                    self.lock.release()
-                    self.lock_counter_1 += 1
-                    print(f' - Замок открылся, во избежании блокировки программы {self.lock_counter_1}-й раз')
+                    self.lock_counter += 1
+                    print(f'     = Замок закрылся {self.lock_counter}-й раз')
             sleep(self.sleep_duration)
 
 
@@ -65,4 +62,4 @@ th1.join()
 th2.join()
 
 print(f'\nИтоговый баланс: {bk.balance}.')
-print(f'Замок блокировки закрывался: {bk.lock_counter_2} раз, открывался: {bk.lock_counter_1} раз.')
+print(f'Замок блокировки закрывался и открывался: {bk.lock_counter} раз.')
