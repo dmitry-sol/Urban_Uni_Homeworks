295956a8414b24ec69b3b9b0d158f3831031960e
dmitry-sol <dmitry-sol@mail.ru>
2024-10-09 20:32:21 +1100

    Add files via upload

---
+++ module_10_5.py
@@ -0,0 +1,30 @@
+# Module_5
+# Многопроцессорное программирование
+
+import multiprocessing
+from datetime import datetime
+
+
+def read_info(file_name):
+    all_data = []
+    with open(file_name, 'r') as file:
+        for line in enumerate(file):
+            line = file.readline()
+            all_data.append(line)
+
+
+if __name__ == '__main__':
+    file_names = [f'./files/file {number}.txt' for number in range(1, 5)]
+
+    # Линейный вызов
+    start = datetime.now()
+    list_ = list(map(read_info, file_names))
+    stop = datetime.now()
+    print(stop-start)
+
+    # Многопроцессорный
+    start = datetime.now()
+    with multiprocessing.Pool(processes=4) as pool:
+        pool.map(read_info, file_names)
+    stop = datetime.now()
+    print(stop - start)
