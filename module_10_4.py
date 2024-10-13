8eece0548c7e6dba4f8e17e5bb4a41506055df98
dmitry-sol <dmitry-sol@mail.ru>
2024-10-14 09:51:34 +1100

    Update module_10_4.py

--- module_10_4.py
+++ module_10_4.py
@@ -33,18 +33,18 @@ class Cafe:
                 if table.guest is None:
                     table.guest = guest
                     guest.start()
-                    print(f'{guest.guest_name} сел(-a) за стол номер {table.number}.')
+                    print(f'{guest.guest_name} сел(-a) за стол номер {table.number}')
                     break
             if not guest.is_alive():
                 self.queue.put(guest)
-                print(f'{guest.guest_name} в очереди.')
+                print(f'{guest.guest_name} в очереди')
 
     def discuss_guests(self):
-        while Cafe.all_tables_occupied(self):
+        while Cafe.any_table_occupied(self):
             for table in self.tables:
                 if table.guest is not None and not table.guest.is_alive():
-                    print(f'{table.guest.guest_name} покушал(-а) и ушел(ушла)')
-                    print(f'Стол номер {table.number} свободен.')
+                    print(f'{table.guest.guest_name} покушал(-а) и ушел(ушла).')
+                    print(f'Стол номер {table.number} свободен')
                     table.guest = None
                     if not self.queue.empty():
                         next_guest = self.queue.get()
@@ -53,13 +53,13 @@ class Cafe:
                         next_guest.start()
                         print(f'{next_guest.guest_name} Вышел(вышла) из очереди и сел(-а) за стол номер {table.number}')
 
-    def all_tables_occupied(self):
-        tables_occupied = False
+    def any_table_occupied(self):
+        table_occupied = False
         for table in self.tables:
             if table.guest is not None:
-                tables_occupied = True
+                table_occupied = True
                 break
-        return tables_occupied
+        return table_occupied
 
 
 tables = [Table(number) for number in range(1, 6)]
