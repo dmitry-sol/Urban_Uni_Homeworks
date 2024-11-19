e465d3f1d6b6ac7e2107d2baa134c2867411724b
dmitry-sol <dmitry-sol@mail.ru>
2024-11-19 19:39:24 +0300

    Update module_13_6.py

--- module_13_6.py
+++ module_13_6.py
@@ -29,7 +29,7 @@ kb_2 = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='м'), KeyboardButton(
 class UserState(StatesGroup):
     sex = State()
     age = State()
-    growth = State()
+    height = State()
     weight = State()
     daily_activity = State()
 
@@ -85,15 +85,15 @@ async def set_age(message, state):
 
 
 @dp.message_handler(state=UserState.age)
-async def set_growth(message, state):
+async def set_height(message, state):
     await state.update_data(age=message.text)
     await message.answer('Введите свой рост')
-    await UserState.growth.set()
+    await UserState.height.set()
 
 
-@dp.message_handler(state=UserState.growth)
+@dp.message_handler(state=UserState.height)
 async def set_weight(message, state):
-    await state.update_data(growth=message.text)
+    await state.update_data(height=message.text)
     await message.answer('Введите свой вес')
     await UserState.weight.set()
 
@@ -118,7 +118,7 @@ async def send_calories(message, state):
     await state.update_data(daily_activity=message.text)
     data = await state.get_data()
     sex_index = 5 if data['sex'] == 'м' else -151
-    calories = round((10 * int(data['weight']) + 6.25 * int(data['growth']) - 5 * int(data['age'])
+    calories = round((10 * int(data['weight']) + 6.25 * int(data['height']) - 5 * int(data['age'])
                       + sex_index) * activity_data[int(data['daily_activity'])])
     await message.answer(f'Ваша норма калорий: {calories}')
     await state.finish()
